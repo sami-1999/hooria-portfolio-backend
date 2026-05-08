515cd1ac2d8450b40c13953950278151a3f4a05f
sami-1999 <muhammad.sami@adalat.info>
2026-05-08 16:35:57 +0500

    fixed vercel issue

--- api/index.js
+++ api/index.js
@@ -1,6 +1,4 @@
-// Vercel serverless function entry point
-// This file wraps the Express app for Vercel deployment
-
-const app = require('../server');
+require('dotenv').config();
+const app = require('../app');
 
 module.exports = app;
